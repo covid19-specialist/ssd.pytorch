c97bd23f0162ef8644a2ac89dd7874320ea5f477
Sadeka Islam <islam.sadeka@gmail.com>
2020-06-11 21:50:35 +0600

    indentation fixed in forward method

--- layers/modules/multibox_loss.py
+++ layers/modules/multibox_loss.py
@@ -142,67 +142,67 @@ class MultiBoxLoss(nn.Module):
         # [num_objs, 5],
 #         [x1,y1,x2,y2,class] format
 
-    loc_data, conf_data, priors = predictions
-    num = loc_data.size(0)
-    priors = priors[:loc_data.size(1), :]
-    num_priors = (priors.size(0))
-    num_classes = self.num_classes
+        loc_data, conf_data, priors = predictions
+        num = loc_data.size(0)
+        priors = priors[:loc_data.size(1), :]
+        num_priors = (priors.size(0))
+        num_classes = self.num_classes
 
-    # match priors (default boxes) and ground truth boxes
-    loc_t = torch.Tensor(num, num_priors, 4)
-    conf_t = torch.LongTensor(num, num_priors)
-    for idx in range(num):
-        truths = targets[idx][:, :-1].data
-        labels = targets[idx][:, -1].data
-        defaults = priors.data
-        match(self.jaccard_thresh, truths, defaults, self.variance, labels,
-              loc_t, conf_t, idx)
-    loc_t = loc_t.cuda()
-    conf_t = conf_t.cuda()
+        # match priors (default boxes) and ground truth boxes
+        loc_t = torch.Tensor(num, num_priors, 4)
+        conf_t = torch.LongTensor(num, num_priors)
+        for idx in range(num):
+            truths = targets[idx][:, :-1].data
+            labels = targets[idx][:, -1].data
+            defaults = priors.data
+            match(self.jaccard_thresh, truths, defaults, self.variance, labels,
+                loc_t, conf_t, idx)
+        loc_t = loc_t.cuda()
+        conf_t = conf_t.cuda()
     
-    # [batch, num_priors]
-    pos = conf_t > 0
-    num_pos = pos.sum(dim=1, keepdim=True)
+        # [batch, num_priors]
+        pos = conf_t > 0
+        num_pos = pos.sum(dim=1, keepdim=True)
 
-    # Localization Loss (Smooth L1)
-    # Shape: [batch,num_priors,4]
-    pos_idx = pos.unsqueeze(pos.dim()).expand_as(loc_data)
-    loc_p = loc_data[pos_idx].view(-1, 4)
-    loc_t = loc_t[pos_idx].view(-1, 4)
-    loss_l = F.smooth_l1_loss(loc_p, loc_t, size_average=False)
+        # Localization Loss (Smooth L1)
+        # Shape: [batch,num_priors,4]
+        pos_idx = pos.unsqueeze(pos.dim()).expand_as(loc_data)
+        loc_p = loc_data[pos_idx].view(-1, 4)
+        loc_t = loc_t[pos_idx].view(-1, 4)
+        loss_l = F.smooth_l1_loss(loc_p, loc_t, size_average=False)
 
-    # Compute max conf across batch for hard negative mining
-    batch_conf = conf_data.view(-1, self.num_classes)
-    loss_c = log_sum_exp(batch_conf) - batch_conf.gather(1, conf_t.view(-1, 1))
+        # Compute max conf across batch for hard negative mining
+        batch_conf = conf_data.view(-1, self.num_classes)
+        loss_c = log_sum_exp(batch_conf) - batch_conf.gather(1, conf_t.view(-1, 1))
 
-    # Hard Negative Mining
-    # [batch, num_priors]
-    loss_c = loss_c.view(num, -1)
-    nonzero_pos = pos.nonzero()
+        # Hard Negative Mining
+        # [batch, num_priors]
+        loss_c = loss_c.view(num, -1)
+        nonzero_pos = pos.nonzero()
 
-    loss_c[pos] = 0  # filter out pos boxes for now
+        loss_c[pos] = 0  # filter out pos boxes for now
 
-    _, loss_idx = loss_c.sort(1, descending=True)
-    _, idx_rank = loss_idx.sort(1)
-    num_pos = pos.long().sum(1, keepdim=True)
-    num_neg = torch.clamp(self.negpos_ratio*num_pos, max=pos.size(1)-1)
-    neg = idx_rank < num_neg.expand_as(idx_rank)
+        _, loss_idx = loss_c.sort(1, descending=True)
+        _, idx_rank = loss_idx.sort(1)
+        num_pos = pos.long().sum(1, keepdim=True)
+        num_neg = torch.clamp(self.negpos_ratio*num_pos, max=pos.size(1)-1)
+        neg = idx_rank < num_neg.expand_as(idx_rank)
     
-    # Confidence Loss Including Positive and Negative Examples
-    pos_idx = pos.unsqueeze(2).expand_as(conf_data)
-    neg_idx = neg.unsqueeze(2).expand_as(conf_data)
+        # Confidence Loss Including Positive and Negative Examples
+        pos_idx = pos.unsqueeze(2).expand_as(conf_data)
+        neg_idx = neg.unsqueeze(2).expand_as(conf_data)
 
-    # .gt(input) computes input > other element-wise.
-    conf_p = conf_data[(pos_idx+neg_idx).gt(0)].view(-1, self.num_classes)
-    targets_weighted = conf_t[(pos+neg).gt(0)]
+        # .gt(input) computes input > other element-wise.
+        conf_p = conf_data[(pos_idx+neg_idx).gt(0)].view(-1, self.num_classes)
+        targets_weighted = conf_t[(pos+neg).gt(0)]
 
-    loss_c = F.cross_entropy(conf_p, targets_weighted, size_average=False)
+        loss_c = F.cross_entropy(conf_p, targets_weighted, size_average=False)
 
-    # Sum of losses: L(x,c,l,g) = (Lconf(x, c) + αLloc(x,l,g)) / N
-    N = num_pos.data.sum().double()
-    loss_l = loss_l.double()
-    loss_c = loss_c.double()
+        # Sum of losses: L(x,c,l,g) = (Lconf(x, c) + αLloc(x,l,g)) / N
+        N = num_pos.data.sum().double()
+        loss_l = loss_l.double()
+        loss_c = loss_c.double()
 
-    loss_l /= N
-    loss_c /= N
-    return loss_l, loss_c
+        loss_l /= N
+        loss_c /= N
+        return loss_l, loss_c
