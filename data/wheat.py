e5a3df6f7ed0b4097c852e4da9aa25b320c95e5e
Sadeka Islam <islam.sadeka@gmail.com>
2020-07-07 23:16:02 +0600

    added background i the label map

--- data/wheat.py
+++ data/wheat.py
@@ -23,7 +23,7 @@ WHEAT_CLASSES = (  # always index 0
 # note: if you used our download scripts, this should be right
 WHEAT_ROOT = "/kaggle/working" #osp.join(HOME, "data/")
 
-label_map = {'wheat': 1}
+label_map = {'background': 0, 'wheat': 1}
 
 rev_label_map = {
     0: 'background',
