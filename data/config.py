223ad59f84e067429447509d65017f60098f8818
Sadeka Islam <islam.sadeka@gmail.com>
2020-07-15 18:10:21 +0600

    cfg 10k iterations

--- data/config.py
+++ data/config.py
@@ -45,7 +45,7 @@ coco = {
 wheat = {
     'num_classes': 2,
     'lr_steps': (10000, 20000, 30000),
-    'max_iter': 30000, #10000,
+    'max_iter': 10000, #10000,
     'feature_maps': [38, 19, 10, 5, 3, 1],
     'min_dim': 300,
     'steps': [8, 16, 32, 64, 100, 300],
