1e097bb5552f442b2f1e82d030ac5304496175af
Sadeka Islam <islam.sadeka@gmail.com>
2020-07-22 20:12:36 +0600

    multiboxloss conf_data code breakdown for debugging in tpu

--- layers/modules/multibox_loss.py
+++ layers/modules/multibox_loss.py
@@ -147,8 +147,8 @@ class MultiBoxLoss(nn.Module):
 #             pos_idx = pos_idx.to(device)
 #             neg_idx = neg_idx.to(device)
             
-        both_idx = (pos_idx+neg_idx).gt(0)
-        
+        both_idx = (pos_idx.float() + neg_idx.float()).gt(0)
+        xm.master_print("both_idx: ", both_idx)
         if both_idx.any():
             conf_p = conf_data[both_idx]
             conf_p = conf_p.view(-1, self.num_classes)
