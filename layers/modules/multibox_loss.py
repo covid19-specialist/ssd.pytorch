ca387606f410c5505b7d5fe811dbd5857fc1e3ac
Sadeka Islam <islam.sadeka@gmail.com>
2020-07-22 14:29:46 +0600

    pos_idx and neg_idx for tpu

--- layers/modules/multibox_loss.py
+++ layers/modules/multibox_loss.py
@@ -141,6 +141,11 @@ class MultiBoxLoss(nn.Module):
         # Confidence Loss Including Positive and Negative Examples
         pos_idx = pos.unsqueeze(2).expand_as(conf_data)
         neg_idx = neg.unsqueeze(2).expand_as(conf_data)
+        
+        if use_gpu == 2:
+            pos_idx = pos_idx.to(device)
+            neg_idx = neg_idx.to(device)
+            
         conf_p = conf_data[(pos_idx+neg_idx).gt(0)].view(-1, self.num_classes)
         targets_weighted = conf_t[(pos+neg).gt(0)]
         ##user_warning
